66193979f9a5b354f71cc270680daff231d19c60
jakubczykwiktor <jakubczykwiktor.szkola@gmail.com>
2026-05-15 14:07:26 +0200

    funkcja ustawiajaca rozmiary aktualnej mapy

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 
 const canvas = document.getElementById("game");
 
-
 const ctx = canvas.getContext("2d");
+
+function resizeCanvas(){
+
+  canvas.width = COLS*TILE;
+
+  canvas.height = ROWS*TILE;
+}

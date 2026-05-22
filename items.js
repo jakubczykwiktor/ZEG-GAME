5977e99598bad60114114017dd10183edc08121d
ABC <ABC>
2026-05-22 14:50:20 +0200

    dodanie przedmiotow na mapie

--- items.js
+++ items.js
@@ -10,3 +10,13 @@ puzzleImg.src = "grafikapienadza.png";
 let hasKey = false;
 let puzzleSolved = false;
 let coins = 0;
+
+function drawItems(ctx){
+  for(let y=0;y<ROWS;y++){
+    for(let x=0;x<COLS;x++){
+      if(currentMap[y][x]===3) ctx.drawImage(keyImg,x*TILE,y*TILE,TILE,TILE);    // 3 = Klucz
+      if(currentMap[y][x]===4) ctx.drawImage(puzzleImg,x*TILE,y*TILE,TILE,TILE); // 4 = Zagadka/Moneta
+      if(currentMap[y][x]===2) ctx.drawImage(doorImg,x*TILE,y*TILE,TILE,TILE);   // 2 = Wyjście
+    }
+  }
+}

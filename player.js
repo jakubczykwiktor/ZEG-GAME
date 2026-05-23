f062c9b6bb2dfcf16501bce2d25ee40ed8c5675c
matus-kwa <dinusek1976@gmail.com>
2026-05-23 23:30:58 +0200

    dodanie pozycji gracza i punktow zycia

--- player.js
+++ player.js
@@ -4,3 +4,10 @@ playerImg.src = "gracz.png";
 
 playerImg.onload = () => console.log("PLAYER OK");
 playerImg.onerror = () => console.log("PLAYER ERROR");
+
+
+let player = {
+  x: 1,  
+  y: 1,  
+  hp: 3  
+};

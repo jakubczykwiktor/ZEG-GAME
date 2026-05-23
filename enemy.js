1824182b0476952c6e9d3a99d75ce0d27364e831
matus-kwa <dinusek1976@gmail.com>
2026-05-23 23:21:38 +0200

    definicja mozliwych kierunkow ruchu węża

--- enemy.js
+++ enemy.js
@@ -11,3 +11,10 @@ let enemy = {
 function drawEnemy(ctx) {
   ctx.drawImage(enemyImg, enemy.x * TILE, enemy.y * TILE, TILE, TILE);
 }
+
+function moveEnemy() {
+  // Definicja możliwych kierunków: góra, dół, lewo, prawo
+  const dirs = [
+    {x:0, y:-1}, {x:0, y:1}, {x:-1, y:0}, {x:1, y:0}
+  ];
+}
